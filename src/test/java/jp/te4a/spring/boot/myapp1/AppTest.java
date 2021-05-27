396885d26547ec36fae0ea3d7d54dcde3296dc94
佐藤 隆太 <180598@jc-21.jp>
2021-05-27 13:35:33 +0900

    test update

--- src/test/java/jp/te4a/spring/boot/myapp1/AppTest.java
+++ src/test/java/jp/te4a/spring/boot/myapp1/AppTest.java
@@ -1,7 +1,7 @@
 package jp.te4a.spring.boot.myapp1;
 
 import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 /**
@@ -16,5 +16,17 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    	
     }
+    
+    @Test
+    public void test_hello()
+    {
+	    String greeting = "Hello, Spring Boot!";
+    	HelloController hellocontroller = new HelloController();
+		String actual =  hellocontroller.index();
+	    assertEquals(greeting,actual);
+    }
+    
+    
 }
